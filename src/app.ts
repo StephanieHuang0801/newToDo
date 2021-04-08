d2a0138b9c38d83f4c35469cedccbbddd1c4ece3
ying0801 <1029476703@qq.com>
2021-04-08 21:50:58 +0800

    初始化入口文件

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+/*
+ * @Author: your name
+ * @Date: 2021-04-08 21:40:58
+ * @LastEditTime: 2021-04-08 21:50:28
+ * @LastEditors: Please set LastEditors
+ * @Description: 模块 立即执行函数
+ * @FilePath: \newToDo\src\app.ts
+ */
+import { ITodoData } from './js/typings'
+    ; ((doc) => {
+        const oInput: HTMLInputElement = document.querySelector('input')
+
+        const oAddBtn: HTMLButtonElement = document.querySelector('button')
+
+        const oTodoList: HTMLElement = document.querySelector('.todo-list')
+        
+        const todoData: ITodoData[] = []
+
+        const init = (): void => {
+
+        }
+        function bindEvent(): void {
+
+        }
+        init()
+    })(document)
